8422b76fc3103be2effd9fa7f7e6d9f7fc55ae93
BElek <timuraliserov20@gmail.com>
2023-05-01 19:11:46 +0600

    feat: homepage verstka

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,12 @@ import airBnb from '../../assets/brand_airbnb.svg'
 import netflix from '../../assets/brand_netflix.svg'
 import slack from '../../assets/brand_slack.svg'
 import vimeo from '../../assets/brand_vimeo.svg'
-import learnMore from '../../assets/iconbase.svg'
+import vector from '../../assets/iconbase.svg'
 import aboutUsIMG from '../../assets//IMG.svg'
-
+import SEO from '../../assets/seo4.svg'
+import emailMarketing from '../../assets/#1.png'
+import Optimization from '../../assets/optimization3.png'
+import socialMarketing from '../../assets/soc.marketing2.png'
 
 
 
@@ -48,7 +51,7 @@ function HomePage() {
               <h2>About Us</h2>
               <h1>Who We Are</h1>
               <p>In hac habitasse platea dictumst. Aliquam lobortis. Lorem <br /> ipsum dolor sit amet, consectetuer adipiscing elit. In dui <br /> magna, posuere eget, vestibulum et, tempor auctor, justo. <br /> Pellentesque habitant morbi tristique senectus et <br /> netus et malesuada fames ac turpis egestas.</p>
-              <button>Learn More <img src={learnMore} alt="learnMore"/></button>
+              <button>Learn More <img src={vector} alt="learnMore"/></button>
             </div>
             <div className={css.aboutUs_works}>
               <div className={css.aboutUs_projects}>
@@ -72,6 +75,40 @@ function HomePage() {
                 </div>
                 <p>Praesent blandit laoreet nibh. Suspendisse pulvinar,<br /> augue ac venenatis condimentum.</p>
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={css.service}>
+          <div className={css.service_desc}>
+            <h2>OUR SERVICES</h2>
+            <h1>We Provide</h1>
+            <p>Nunc nonummy metus. Donec elit libero, sodales nec, <br /> volutpat a, suscipit non, turpis.</p>
+          </div>
+          <div className={css.service_wrapper}>
+            <div className={css.service_cardSEO}>
+              <img src={SEO} alt="SEO" />
+              <h1>SEO</h1>
+              <p>Nunc nonummy metus. Donec elit libero</p>
+              <img src={vector} alt="vector" />
+            </div>
+            <div className={css.service_cardMarketing}>
+              <img src={emailMarketing} alt="emailMarketing" />
+              <h1>Email Marketing</h1>
+              <p>Nunc nonummy metus. Donec elit libero</p>
+              <img src={vector} alt="vector" />
+            </div>
+            <div className={css.service_cardOptimization}>
+              <img src={Optimization} alt="Optimization" />
+              <h1>Search Engine <br /> Oprimization</h1>
+              <p>Nunc nonummy metus. Donec elit libero</p>
+              <img src={vector} alt="vector" />
+            </div>
+            <div className={css.service_cardSocialMarket}>
+              <img src={socialMarketing} alt="socialMarketing" />
+              <h1>Social Marketing</h1>
+              <p>Nunc nonummy metus. Donec elit libero</p>
+              <img src={vector} alt="vector" />
             </div>
           </div>
         </section>
